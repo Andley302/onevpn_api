324dcf42de439ec196d43f2e89b370e82fd193e6
Andley S <andley.silvabr@gmail.com>
2022-12-05 00:10:19 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -273,7 +273,31 @@ service cron reload;
 #BADVPN
 clear;
 echo "Aguarde...";
+sleep 3;
+clear;
+echo "Banner SSH...";
+sleep 3;
+cd /etc;
+wget https://raw.githubusercontent.com/Andley302/clearssh/main/ssh/bannerssh;
+cd /root;
+clear;
+echo "Instalando Dropbear...";
 sleep 5;
+porta=8080;
+apt install dropbear -y;
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear >/dev/null 2>&1
+sed -i "s/DROPBEAR_PORT=22/DROPBEAR_PORT=$porta/g" /etc/default/dropbear >/dev/null 2>&1
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 8000"/g' /etc/default/dropbear >/dev/null 2>&1
+sed -i 's/DROPBEAR_BANNER=""//g' /etc/default/dropbear >/dev/null 2>&1
+sed -i "$ a DROPBEAR_BANNER=\"/etc/bannerssh\"" /etc/default/dropbear;
+grep -v "^PasswordAuthentication yes" /etc/ssh/sshd_config >/tmp/passlogin && mv /tmp/passlogin /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >>/etc/ssh/sshd_config
+grep -v "^PermitTunnel yes" /etc/ssh/sshd_config >/tmp/ssh && mv /tmp/ssh /etc/ssh/sshd_config
+echo "PermitTunnel yes" >>/etc/ssh/sshd_config
+echo "/bin/false" >>/etc/shells
+service dropbear restart;
+service dropbear stop;
+service dropbear start;
 clear;
 echo "Deseja instalar o badvpn? (s/n)"
 read CONFIRMA
@@ -396,10 +420,10 @@ clear;
 echo "Finalizando...";
 cd /root;
 sleep 5;
-service dropbear stop;
-service dropbear start;
 rm -rf installer.sh;
 rm -rf fast_linux_amd64;
+apt autoremove - y;
+apt autoclean;
 clear;
 echo "FIM!";
 sleep 5;
